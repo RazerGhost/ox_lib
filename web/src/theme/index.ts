7166ace3542acb7f511d67628a193aa3aaab26c8
RazerGhost <IRazerIGhost@gmail.com>
2024-01-09 14:52:07 +0100

    changes for anna

--- web/src/theme/index.ts
+++ web/src/theme/index.ts
@@ -2,6 +2,21 @@ import { MantineThemeOverride } from '@mantine/core';
 
 export const theme: MantineThemeOverride = {
   colorScheme: 'dark',
+  colors: {
+    // override dark colors to change them for all components
+    dark: [
+      '#C1C2C5',
+      '#A6A7AB',
+      '#909296',
+      '#5c5f66',
+      '#373A40',
+      '#087f5bE6',
+      '#060608',
+      '#060608',
+      '#141517',
+      '#101113',
+    ],
+  },
   fontFamily: 'Roboto',
   shadows: { sm: '1px 1px 3px rgba(0, 0, 0, 0.5)' },
   components: {
